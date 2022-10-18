d4928dd3e9d4489eaef795183909de150a0e9ff7
chewzzz <chewziqing@gmail.com>
2022-10-18 22:44:38 +0800

    Add lows temp plot and shading

--- pygal/weather-plot/highs_lows.py
+++ pygal/weather-plot/highs_lows.py
@@ -22,20 +22,26 @@ with open(filename) as f:
     header_row = next(reader)
 
     # extracting high temperature
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:  # loop through all rows
         date = datetime.strptime(row[0], '%Y-%m-%d') # get the first item (date) of every row
         dates.append(date)
 
         highs.append(int(row[1]))    # get the second item (high temperature) of every row
+
+        low = int(row[3])
+        lows.append(low)
+
     print(highs)
 
 
 # visualize the high temperature
 fig = plt.figure(dpi=128, figsize=(6, 4))
-plt.plot(dates, highs, c="red")
+plt.plot(dates, highs, c="red", alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
-plt.title("Daily High Temperature, July 2014", fontsize=16)
+plt.title("Daily High and Low Temperature, July 2014", fontsize=16)
 plt.xlabel("", fontsize=12)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=12)
